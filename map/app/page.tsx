be9fd9dbf95902408cf3c3b632b9c3f76039eb43
Yuhei Onishi <onisan0313@gmail.com>
2026-04-26 18:26:32 +0900

    page.tsx の修正

--- map/app/page.tsx
+++ map/app/page.tsx
@@ -1,4 +1,8 @@
-import Map from './components/Map'
+'use client'
+
+import dynamic from 'next/dynamic'
+
+const Map = dynamic(() => import('./components/Map'), { ssr: false })
 
 export default function Home() {
   return (
